4e67de57e43072bc5308726380ec2c438ce866cc
Jan Vrany <jan.vrany@fit.cvut.cz>
2020-11-20 09:53:27 +0000

    Configure GDB with `--enable-targets=all`
    ...to make sure everyone is satisfied.

--- config/includes.chroot/var/lib/live/install-gdb.sh
+++ config/includes.chroot/var/lib/live/install-gdb.sh
@@ -13,7 +13,7 @@ apt-get -y install \
 rm -rf /tmp/binutils-gdb
 
 git clone --depth=1 "${repo}" /tmp/binutils-gdb
-(cd /tmp/binutils-gdb && ./configure --prefix=/opt/gdb --disable-werror --with-guile=no --with-python=/usr/bin/python3 --enable-targets="i686-pc-linux-gnu,x86_64-pc-linux-gnu,i686-w64-mingw32,x86_64-w64-mingw32,riscv64-pc-linux-gnu,ppc-pc-linux-gnu")
+(cd /tmp/binutils-gdb && ./configure --prefix=/opt/gdb --disable-werror --with-guile=no --with-python=/usr/bin/python3 --enable-targets=all)
 make -C /tmp/binutils-gdb -j2
 make -C /tmp/binutils-gdb install
 
